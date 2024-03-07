87faec46e5f2b5e6a24cb12c74ab6152b56e2c48
Muhammad Raza <hmrazarafique@gmail.com>
2024-03-06 20:18:26 -0500

    Styled home page, created other pages and links to them

--- main.py
+++ main.py
@@ -6,4 +6,16 @@ app = Flask(__name__)
 def home_page():
     return render_template("home.html")
 
-app.run()
+@app.route("/time")
+def time_page():
+    return render_template("time.html")
+
+@app.route("/words")
+def words_page():
+    return render_template("words.html")
+
+@app.route("/settings")
+def settings_page():
+    return render_template("settings.html")
+    
+app.run(debug=True)
