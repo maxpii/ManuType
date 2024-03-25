dd43ec8d54d1e109cee1de7264c22c9180b28a6a
Muhammad Raza <hmrazarafique@gmail.com>
2024-03-24 23:20:08 -0400

    writing user input

--- static/scripts/words.js
+++ static/scripts/words.js
@@ -1,15 +1,23 @@
 let word_array = []
 let word = document.getElementById("word");
 let counter = document.getElementById("count");
-
+let input = document.getElementById("textbox");
 function filter(data) {
     for (let i = 0; i < data.length; i++) {
         word_array.push(data[i]);
       }
 }
 
+function textHandler() {
+    var currentValue = input.value;
+    if (currentValue.charAt(currentValue.length-1) === " ") {
+      input.value = "";
+    }
+}
+
+
 fetch("https://random-word-api.herokuapp.com/word?number=25")
 .then(res => res.json())
 .then(data => filter(data));
 
-console.log(word_array);
+textHandler();
