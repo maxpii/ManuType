a3130ecaaf1a7d2416f81948615297d95783bff4
Muhammad Raza <hmrazarafique@gmail.com>
2024-05-10 23:11:42 -0400

    Working on database

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "7c9a3daa9593e08a5cd46a6c85e2bdf2"
-app.config["SQLALCHEMY_DATABASE_URL"] = 'sqlite:///site.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False) #string of 20 characters, and cannot be duplicated
+    email = db.Column(db.String(120), unique=True, nullable=False) #string of 20 characters, and cannot be duplicated
+    image_file = db.Column(db.String(20),nullable=False, default='default.jpg') #string of 20 characters, and cannot be duplicated
+    password = db.Column(db.String(60), nullable=False)
+    #TODO:create columns for typing-specific data
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
 
 output = []
 
